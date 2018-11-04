2aaf46686c3ac7a24e996dc9cf0e43377f6aa9be
Saeed Ganji <shahabganji@gmail.com>
2018-11-04 13:53:27 +0330

    replace(css-loader)
    Replace "extract-text-webpack-plugin" with "MiniCssExtractPlugin"

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
 const path = require("path");
 const webpack = require("webpack");
-const { AureliaPlugin, ModuleDependenciesPlugin, GlobDependenciesPlugin } = require("aurelia-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const {
+	AureliaPlugin,
+	ModuleDependenciesPlugin,
+	GlobDependenciesPlugin
+} = require("aurelia-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const extractCSS = new ExtractTextPlugin("vendor.css");
 const bundleOutputDir = "./wwwroot/dist";
+const cssRules = [{
+	loader: 'css-loader'
+}, ];
 
-module.exports = (env, argv) => {
+module.exports = (env, argv, extractCss) => {
 	if ((!argv || !argv.mode) && process.env.ASPNETCORE_ENVIRONMENT === "Development") {
-		argv = { mode: "development" };
+		argv = {
+			mode: "development"
+		};
 	}
 	console.log("mode=", argv.mode);
 	const isDevBuild = argv.mode !== "production";
-	const cssLoader = { loader: isDevBuild ? "css-loader" : "css-loader?minimize" };
+
 	return [{
 		target: "web",
 		mode: isDevBuild ? "development" : "production",
-		entry: { "app": ["es6-promise/auto", "aurelia-bootstrapper"] },
+		entry: {
+			"app": ["es6-promise/auto", "aurelia-bootstrapper"]
+		},
 		resolve: {
 			extensions: [".ts", ".js"],
 			modules: ["ClientApp", "node_modules"]
@@ -28,16 +38,55 @@ module.exports = (env, argv) => {
 			chunkFilename: "[name].js"
 		},
 		module: {
-			rules: [
-				{ test: /\.(woff|woff2)(\?|$)/, loader: "url-loader?limit=1" },
-				{ test: /\.(png|eot|ttf|svg)(\?|$)/, loader: "url-loader?limit=100000" },
-				{ test: /\.ts$/i, include: [/ClientApp/, /node_modules/], use: "awesome-typescript-loader" },
-				{ test: /\.html$/i, use: "html-loader" },
-				{ test: /\.css(\?|$)/, include: [/node_modules/], loader: extractCSS.extract([isDevBuild ? 'css-loader' : 'css-loader?minimize']) },
-				{ test: /\.css$/i, exclude: [/node_modules/], issuer: /\.html$/i, use: cssLoader },
-				{ test: /\.css$/i, exclude: [/node_modules/], issuer: [{ not: [{ test: /\.html$/i }] }], use: ["style-loader", cssLoader] },
-				{ test: /\.scss$/i, issuer: /(\.html|empty-entry\.js)$/i, use: [cssLoader, "sass-loader"] },
-				{ test: /\.scss$/i, issuer: /\.ts$/i, use: ["style-loader", cssLoader, "sass-loader"] }
+			rules: [{
+					test: /\.(woff|woff2)(\?|$)/,
+					loader: "url-loader?limit=1"
+				},
+				{
+					test: /\.(png|eot|ttf|svg)(\?|$)/,
+					loader: "url-loader?limit=100000"
+				},
+				{
+					test: /\.ts$/i,
+					include: [/ClientApp/, /node_modules/],
+					use: "awesome-typescript-loader"
+				},
+				{
+					test: /\.html$/i,
+					use: "html-loader"
+				},
+				{
+					test: /\.css$/i,
+					issuer: [{
+						not: [{
+							test: /\.html$/i
+						}]
+					}],
+					use: extractCss ? [{
+							loader: MiniCssExtractPlugin.loader
+						},
+						'css-loader'
+					] : ['style-loader', ...cssRules]
+				},
+				{
+					test: /\.css$/i,
+					issuer: [{
+						test: /\.html$/i
+					}],
+					// CSS required in templates cannot be extracted safely
+					// because Aurelia would try to require it again in runtime
+					use: cssRules
+				},
+				{
+					test: /\.scss$/,
+					use: ['style-loader', 'css-loader', 'sass-loader'],
+					issuer: /\.[tj]s$/i
+				},
+				{
+					test: /\.scss$/,
+					use: ['css-loader', 'sass-loader'],
+					issuer: /\.html?$/i
+				},
 			]
 		},
 		optimization: {
@@ -56,12 +105,25 @@ module.exports = (env, argv) => {
 			hints: false
 		},
 		plugins: [
-			new webpack.DefinePlugin({ IS_DEV_BUILD: JSON.stringify(isDevBuild) }),
-			new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery", "window.jQuery": "jquery" }),
-			new AureliaPlugin({ aureliaApp: "boot" }),
-			new GlobDependenciesPlugin({ "boot": ["ClientApp/**/*.{ts,html}"] }),
+			new webpack.DefinePlugin({
+				IS_DEV_BUILD: JSON.stringify(isDevBuild)
+			}),
+			new webpack.ProvidePlugin({
+				$: "jquery",
+				jQuery: "jquery",
+				"window.jQuery": "jquery"
+			}),
+			new AureliaPlugin({
+				aureliaApp: "boot"
+			}),
+			new GlobDependenciesPlugin({
+				"boot": ["ClientApp/**/*.{ts,html}"]
+			}),
 			new ModuleDependenciesPlugin({}),
-			extractCSS
+			new MiniCssExtractPlugin({
+				filename: !isDevBuild /*production*/ ? '[contenthash].css' : '[id].css',
+				allChunks: true
+			})
 		]
 	}];
 };
